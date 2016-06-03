2a1d3f6ebe0d49a02aa0f86fcb4e946b0aeec736
V_ <v_huyang01@internal.baidu.com>
2016-06-03 20:08:51 +0800

    f

---
+++ lvyuan-common/src/main/java/net/hoyoung/persistent/HibernateUtils.java
@@ -0,0 +1,35 @@
+package net.hoyoung.persistent;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtils {
+	private static volatile SessionFactory sessionFactory;
+	public static SessionFactory getSessionFactory(){
+		if(sessionFactory==null){
+            synchronized (HibernateUtils.class){
+                if(sessionFactory==null){
+                    Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+                    ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+                    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                }
+            }
+		}
+		return sessionFactory;
+	}
+	public static Session openSession(){
+		if(sessionFactory==null){
+			getSessionFactory();
+		}
+		return sessionFactory.openSession();
+	}
+    public static Session getCurrentSession(){
+		if(sessionFactory==null){
+			getSessionFactory();
+		}
+		return sessionFactory.getCurrentSession();
+	}
+}
